ea26d62e561d08639aa75986c2d5e59199cf71df
learpcs <learpcs@gmail.com>
2024-02-27 17:15:50 +0400

    kek...

--- java-tasks/src/Main.java
+++ java-tasks/src/Main.java
@@ -6,14 +6,5 @@ public class Main {
         catch (Exception e) {
             System.out.println(e);
         }
-
-        ExampleClass a1 = new ExampleClass(1, 'a' , new int[1]);
-        ExampleClass a2 = new ExampleClass(2, 'a' , new int[1]);
-        ExampleClass a3 = new ExampleClass(3, 'a' , new int[1]);
-
-        ExampleClass[] arr = {a1, a2, a3};
-        Object[] b = {a1, a3, a2};
-        arr = (ExampleClass[])b;
-        System.out.println(arr[0]);
     }
 }
